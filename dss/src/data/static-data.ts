60d2b15f5371310e30c9208812bc197fc8d08400
alissonbk <alissonbk2010@hotmail.com>
2023-09-20 16:01:40 -0300

    using equations

--- dss/src/data/static-data.ts
+++ dss/src/data/static-data.ts
@@ -1,5 +1,5 @@
 import { EqResult } from 'src/model/eq-result';
-import { InputsT, MeasurementUnitT } from './../model/utils';
+import { InputsT, MeasurementUnitT, StatesT } from './../model/utils';
 import { Equation } from "src/model/equation";
 
 function Log (num: number) {
@@ -10,40 +10,43 @@ export const EQUATIONS: Set<Equation> = new Set([
 
   new Equation(
       '',                                                    // Textural Class
-      ['bulkDensity'],                                       // Inputs Accepted
+      [InputsT.bulkDensity],                                       // Inputs Accepted
       (bd: number): EqResult => {                            // Equation
         const result = (0.46082 - 0.18014 * bd);
         return new EqResult(result, 'cm/cm')
       },
       '',                                                    // Class
-      ['BA'],                                                // States
-      0
+      [StatesT.BA],                                                // States
+      0,
+      'fieldCapacity'
   ),
 
   new Equation(
     '',                                                          // Textural Class
     //FIXME: VERIFICAR SE CS = claySilt ou coarseSand
-    ['sand', 'claySilt', 'liquidLimits'],                        // Inputs Accepted
+    [InputsT.sand, InputsT.claySilt, InputsT.liquidLimits],                        // Inputs Accepted
     (sand: number, CS: number, LL: number): EqResult => {        // Equation
       const result = (169 + 17.1 * Log((sand/10)) - 17.5 * Log((CS/10)) + 0.53 * LL);
       return new EqResult(result, 'g/kg')
     },
     '',                                                         // Class
-    ['RS', 'SC'],                                               // States
-    0
+    [StatesT.RS, StatesT.SC],                                               // States
+    0,
+    'fieldCapacity'
   ),
 
   new Equation(
     '',                                                          // Textural Class
     //FIXME: VERIFICAR SE CS = claySilt ou coarseSand
-    ['organicMatter', 'claySilt', 'liquidLimits'],               // Inputs Accepted
+    [InputsT.organicMatter, InputsT.claySilt, InputsT.liquidLimits],               // Inputs Accepted
     (OM: number, CS: number, LL: number): EqResult => {          // Equation
       const result = (179 + 13 * Log((OM/10)) - 23.6 * Log((CS/10)) + 0.53 * LL);
       return new EqResult(result, 'g/kg')                       // Measurement Unit
     },
     '',                                                         // Class
-    ['RS', 'SC'],                                               // States
-    0
+    [StatesT.RS, StatesT.SC],                                               // States
+    0,
+    'fieldCapacity'
   ),
 
 ]);
