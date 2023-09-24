2649fbb7b8d1b8beaf5ab80bc9f860a71fa2345c
alissonbk <alissonbk2010@hotmail.com>
2023-09-24 15:15:13 -0300

    fix: cmmts

--- dss/src/data/static-data.ts
+++ dss/src/data/static-data.ts
@@ -11,45 +11,47 @@ function Log (num: number) {
 export const EQUATIONS: Set<Equation> = new Set([
 
   new Equation(
-      '',                                                    // Textural Class
-      [InputsT.bulkDensity],                                       // Inputs Accepted
-      (bd: number): EqResult => {                            // Equation
+      '',                                                                 // Textural Class
+      [InputsT.bulkDensity],                                              // Inputs Accepted
+      (bd: number): EqResult => {                                         // Equation
         const result = (0.46082 - 0.18014 * bd);
         return new EqResult(result, 'cm/cm')
       },
-      '',                                                    // Class
-      [StatesT.BA],                                                // States
-      0,
+      '',                                                                 // Class
+      [StatesT.BA],                                                       // States
+      0,                                                                  //RMSE
       'fieldCapacity'
   ),
 
   new Equation(
-    '',                                                          // Textural Class
+    '',                                                                   // Textural Class
     //FIXME: VERIFICAR SE CS = claySilt ou coarseSand
-    [InputsT.sand, InputsT.coarseSand, InputsT.liquidLimits],                        // Inputs Accepted
-    (CS: number, sand: number, LL: number): EqResult => {        // Equation
-      const result = (169 + 17.1 * Log((sand/10)) - 17.5 * Log((CS/10)) + 0.53 * LL);
+    [InputsT.sand, InputsT.coarseSand, InputsT.liquidLimits],             // Inputs Accepted
+    (CS: number, sand: number, LL: number): EqResult => {                 // Equation
+      const result =
+        (169 + 17.1 * Log((sand/10)) - 17.5 * Log((CS/10)) + 0.53 * LL);
       return new EqResult(result, 'g/kg')
     },
-    '',                                                         // Class
-    [StatesT.RS, StatesT.SC],                                               // States
-    0,
-    'fieldCapacity'
+    '',                                                                   // Class
+    [StatesT.RS, StatesT.SC],                                             // States
+    0,                                                                    // RMSE
+    'fieldCapacity'                                                       // TYPE
   ),
 
   new Equation(
-    '',                                                          // Textural Class
+    '',                                                                   // Textural Class
     //FIXME: VERIFICAR SE CS = claySilt ou coarseSand
-    [InputsT.organicMatter, InputsT.coarseSand, InputsT.liquidLimits],               // Inputs Accepted
-    (CS: number, LL: number, OM: number): EqResult => {          // Equation
+    [InputsT.organicMatter, InputsT.coarseSand, InputsT.liquidLimits],    // Inputs Accepted
+    (CS: number, LL: number, OM: number): EqResult => {                   // Equation
       console.log("OM: ", OM, "CS:", CS, "LL: ", LL)
-      const result = (179 + 13 * Log((OM/10)) - 23.6 * Log((CS/10)) + 0.53 * LL);
-      return new EqResult(result, 'g/kg')                       // Measurement Unit
+      const result =
+        (179 + 13 * Log((OM/10)) - 23.6 * Log((CS/10)) + 0.53 * LL);
+      return new EqResult(result, 'g/kg')                                 // Measurement Unit
     },
-    '',                                                         // Class
-    [StatesT.RS, StatesT.SC],                                               // States
-    0,
-    'fieldCapacity'
+    '',                                                                   // Class
+    [StatesT.RS, StatesT.SC],                                             // States
+    0,                                                                    // RMSE
+    'fieldCapacity'                                                       // TYPE
   ),
 
 ]);
