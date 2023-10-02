f3edb7479be0c9372c89b500e82075517da76494
Inácio <67322986+Inaciocb@users.noreply.github.com>
2023-10-02 00:22:08 -0300

    Update eq-result.ts

--- dss/src/model/eq-result.ts
+++ dss/src/model/eq-result.ts
@@ -1,9 +1,11 @@
+import { EqType } from "./equation";
 import { MeasurementUnitT } from "./utils";
 
 export type EqFunction = (...inputs: number[]) => EqResult;
 export class EqResult {
   result: number;
   measurementUnit: MeasurementUnitT;
+  eqType!: EqType;
 
   constructor(_result: number, _measurementUnit: MeasurementUnitT) {
     this.result = _result;
@@ -11,6 +13,19 @@ export class EqResult {
   }
 
   toString(): string {
-    return this.result + ' ' + this.measurementUnit;
+    console.log(this.eqType);
+    let type = ""
+    switch(this.eqType) {
+      case 'fieldCapacity':
+        type = 'Capacidade de Campo: '
+        break;
+      case 'permanentWiltingPoint':
+        type = 'Ponto de Murcha Permanente: '
+        break;
+      default:
+        type = '';
+    }
+
+    return type + this.result + ' ' + this.measurementUnit;
   }
 }
